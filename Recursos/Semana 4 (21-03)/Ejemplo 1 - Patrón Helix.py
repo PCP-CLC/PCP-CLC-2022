7a89a389212b862801ac16070c3dc643aa1fdc63
PCP-CLC <100717858+PCP-CLC@users.noreply.github.com>
2022-03-19 13:08:17 -0300

    Add files via upload

---
+++ Recursos/Semana 4 (21-03)/Ejemplo 1 - Patrón Helix.py
@@ -0,0 +1,13 @@
+# Programa para dibujar patrón helix usando libreria Turtle
+
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x//100 + 1)
+    t.forward(x)
+    t.left(59)
+
+turtle.exitonclick()

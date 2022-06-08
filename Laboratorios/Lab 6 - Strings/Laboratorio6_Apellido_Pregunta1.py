a317413f38af8413e186217b08168b5bc26c46a9
PCP-CLC <100717858+PCP-CLC@users.noreply.github.com>
2022-06-08 08:13:13 -0400

    Add files via upload

---
+++ Laboratorios/Lab 6 - Strings/Laboratorio6_Apellido_Pregunta1.py
@@ -0,0 +1,25 @@
+#la variable texto sirve probar su programa
+texto = "pero antes de llegar a chile hice otro descubrimiento \
+que agregaria un nuevo estrato al desarrollo de mi Poesía. me detuve en el \
+perú y subí hAsta las ruinas de macchu picchu. ascendimos a caBallo. \
+por entonces no había carretera. desde lo aLto vi las antiguas cOnstrucciones \
+de piedra rodeadas por las altísimas cumbres de los andes verdes. desde la \
+ciudadela carcomida y roída por el paso de los siglos se despeñaban_ \
+torrentes. masas de Neblina blanca se levantaban desde el río wilcamayo. \
+me sentí infinitamente pequeño en el cEntro de aquel ombligo de piedra; \
+ombligo de un mundo deshabitado, oRgulloso y eminente, al que de algún modo \
+yo pertenecía. sentí que mis propias manos habían trabajado allí en algUna \
+etapa lejana, cavando surcos, alisando peñascos. me sentí chileno, peruano, \
+americano. había encontrado en aquellas alturas difíciles, entre aquellas \
+ruinas gloriosas y Dispersas, una profesión de fe para la continuación de \
+mi canto. allí nació mi poema alturAs de macchu picchu"
+
+def decodificador (mensaje):
+    mendeco = "" #En esta variable debes guardar el mensaje oculto
+    #Aqui comienza tu desarrollo
+
+
+    #Aquí termina tu desarrollo
+    return mendeco
+
+print(decodificador(texto))

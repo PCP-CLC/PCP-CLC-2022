e9eaba5e00c0339ff9a96e8d7ef33ab43a06dd97
PCP-CLC <100717858+PCP-CLC@users.noreply.github.com>
2022-08-11 09:45:04 -0400

    Add files via upload

---
+++ Tareas/Tarea 2/Tarea3_Apellido.py
@@ -0,0 +1,12 @@
+import PCPictures_gui
+
+
+def tarea():
+	# Aqui empieza tu tarea
+	pass
+#Aqui termina su tarea
+
+app = PCPictures_gui.Application("tarea")
+app.title('PCPictures CLC 2022')
+app.loadProgram(tarea)
+app.start()
